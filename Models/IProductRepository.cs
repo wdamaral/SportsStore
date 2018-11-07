7823cc5d509598c3ac7e5f834cafedf6c237888f
Wagner D'Amaral <32019817+wdamaral@users.noreply.github.com>
2018-11-07 09:28:31 -0500

    First commit

---
+++ Models/IProductRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportsStore.Models
+{
+    public class IProductRepository
+    {
+        IQueryable<Product> Products { get; }
+    }
+}
